63587879a4105d7a84da52293eec0d0698070c5d
Ellen Kramp <ellenkramp@gmail.com>
2024-11-22 21:40:52 -0500

    add lambda layers

---
+++ packages/lambda-layers/utils-lambda-layer/src/gatewayResponse.ts
@@ -0,0 +1,26 @@
+import * as lambda from "aws-lambda";
+
+const createGatewayResponse = ({
+  statusCode,
+  body,
+}: {
+  statusCode: number;
+  body: string;
+}): lambda.APIGatewayProxyResult => {
+  return {
+    statusCode,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+      "Access-Control-Allow-Headers": "*",
+      "Access-Control-Allow-Methods": "*",
+    },
+    body,
+  };
+};
+
+export const createSuccessJsonResponse = (body: string) =>
+  createGatewayResponse({ statusCode: 200, body: JSON.stringify(body) });
+
+export const createErrorJsonResponse = (body: string) =>
+  createGatewayResponse({ statusCode: 500, body: JSON.stringify(body) });
